7064f975dbb7376459391316b1f61840e260e5f5
CinArb2 <91969387+CinArb2@users.noreply.github.com>
2022-02-18 10:12:09 -0500

    arreglado navbar

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 let btnMenu = document.querySelector('.boton');
 let navBar = document.querySelector('nav');
 let body = document.querySelector('body');
-let scrollBtn = document.querySelector('.scroll-up')
+let scrollBtn = document.querySelector('.scroll-up');
+
+
+
 
 btnMenu.addEventListener('click', () => {
   navBar.classList.toggle('visible');
@@ -9,13 +12,20 @@ btnMenu.addEventListener('click', () => {
   body.classList.toggle('overflow')
 })
 
-window.addEventListener('scroll', e => {
+window.addEventListener('scroll', () => {
 
   if (window.scrollY > 250) {
     btnMenu.style.display = 'none';
-    scrollBtn.classList.add('translate')
-  } else if(window.scrollY <= 250){
+    navBar.classList.remove('visible');
+    scrollBtn.classList.add('translate');
+  } else {
     btnMenu.style.display = 'block';
     scrollBtn.classList.remove('translate')
   }
+  body.classList.remove('overflow');
+  btnMenu.classList.remove('visible');
 })
+
+
+
+
